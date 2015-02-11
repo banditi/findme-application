6f7603dd8a5b3fb5e6c2a52a786c2761dffa37b6
Unconditionally <pechenkaya@mail.ru>
2015-02-11 22:50:38 +0300

    VK auth attempt

--- application/findme/app/src/main/java/com/master_igor/findme/MainActivity.java
+++ application/findme/app/src/main/java/com/master_igor/findme/MainActivity.java
@@ -1,17 +1,50 @@
 package com.master_igor.findme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKSdkListener;
+import com.vk.sdk.VKUIHelper;
+import com.vk.sdk.api.VKError;
+
 
 public class MainActivity extends Activity {
 
+    private static String appId = "4777396";
+    private static String tokenKey = "5E27kyO4tAKdJdUaVy67";
+    private static String[] sMyScope = new String[]{VKScope.FRIENDS, VKScope.MESSAGES,
+            VKScope.STATUS, VKScope.WALL, VKScope.NOHTTPS};
+
+
+    VKSdkListener listener = new VKSdkListener() {
+        @Override
+        public void onCaptchaError(VKError captchaError) {
+
+        }
+
+        @Override
+        public void onTokenExpired(VKAccessToken expiredToken) {
+
+        }
+
+        @Override
+        public void onAccessDenied(VKError authorizationError) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        VKSdk.initialize(listener, appId, VKAccessToken.tokenFromSharedPreferences(this, tokenKey));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VKSdk.authorize(sMyScope);
     }
 
 
@@ -35,5 +68,25 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*VK methods*/
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        VKUIHelper.onResume(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VKUIHelper.onDestroy(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
     }
 }
