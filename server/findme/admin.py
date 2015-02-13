9632e0c3fe43ce4e8abbf20b951dcdde4dcf5a21
Sigorilla <sigorilla@gmail.com>
2015-02-14 01:16:32 +0300

    add some function :fast_forward:
    * add ID
    * get list of friends by ID
    * set coordinates by ID, latitude and longitude
    * set status (online/offline) by ID

--- server/findme/admin.py
+++ server/findme/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
 from findme.models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    '''
+        Admin View for User
+    '''
+    list_display = ('name', 'idvk', 'online')
+    readonly_fields = ('name', 'idvk')
+    search_fields = ['name', 'idvk']
+
+admin.site.register(User, UserAdmin)
