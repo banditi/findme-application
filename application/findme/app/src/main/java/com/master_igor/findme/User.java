54777f2b977ba0e235abe4643a6e204a4038e1cb
Nipel-Crumple <warlen94@mail.ru>
2015-02-14 19:17:55 +0300

    List of friends works

--- application/findme/app/src/main/java/com/master_igor/findme/User.java
+++ application/findme/app/src/main/java/com/master_igor/findme/User.java
@@ -4,25 +4,49 @@ import org.joda.time.DateTime;
 
 public class User {
 
-    private final String name;
-    private final DateTime birthDate;
-    private final String dateFormat;
+    private String name;
+    private int idvk;
+    private double latitude;
+    private double longitude;
+    private int distance;
 
-    public User(String name, DateTime birthDate, String dateFormat) {
+    public User(String name) {
         this.name = name;
-        this.birthDate = birthDate;
-        this.dateFormat = dateFormat;
-    }
-
-    public DateTime getBirthDate() {
-        return birthDate;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDateFormat() {
-        return dateFormat;
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getIdvk() {
+        return idvk;
+    }
+
+    public void setIdvk(int idvk) {
+        this.idvk = idvk;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
     }
 }
